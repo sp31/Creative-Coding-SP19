c196ea264e3383a4228004991d81538ccb30ae30
SP <sp31@users.noreply.github.com>
2019-05-06 18:50:35 -0700

    Final commit of final project.

--- Homework/final/sketch.js
+++ Homework/final/sketch.js
@@ -24,10 +24,11 @@ var whippedCream;
 var iceCream;
 var instructions;
 var currentImage = -1;
+var plusOne = "+1";
 
 var startTimer = false;
 var prevTime = 0;
-var interval = 3000;
+var interval = 1000;
 var rightWrong = false;
 
 let timer = 10
@@ -36,7 +37,6 @@ let timerThree = 15
 
 var baristaHead;
 var baristaColor = '#eeee00';
-var latteStages = [];
 var answer = "";
 
 
@@ -49,6 +49,8 @@ var coffeeType = [[4,3,1], [1,2,4],[3,4,2]];
 var machClick = 0;
 var lattClick = 0;
 var glacClick = 0;
+
+new p5.SoundFile("assets/coffeesound.mp3", [successCallback], [errorCallback], [whileLoadingCallback]);
 
 
 function preload(){
@@ -73,6 +75,11 @@ function preload(){
   whippedCream = loadImage("assets/whippedcream.png");
   iceCream = loadImage("assets/icecream.png");
   instructions = loadImage("assets/instructions.png");
+  congrats = loadImage("assets/congrats.png");
+
+  soundFormats('mp3', 'ogg');
+    coffeeSound = loadSound("assets/coffeesound.mp3");
+
 
   machStages = [machZero, machOne, machTwo, machThree];
   latteStages = [lattZero, lattOne, lattTwo, lattThree];
@@ -83,6 +90,8 @@ function setup() {
   // put setup code here
 createCanvas(700,624);
 
+coffeeSound.setVolume(0.1);
+  coffeeSound.play();
 
 
 buttonOne = createButton("Order I: Macchiato");
@@ -126,34 +135,36 @@ function draw() {
 
   }
 
-//GUI
-  // colorMode(0, 90, 1);
-  // gui.addGlobals('baristaColor');
-  // noLoop();
+
 
 
   if(currentImage == 0){
     image(cafeScene,0,0);
-    image(machStages[0],80,500);
+    image(machStages[machClick],80,500);
     image(milk,230,500);
     image(espresso,370,500);
     image(syrup,505,500);
     text("Caramel Macchiato",80,615);
+    text(plusOne,270,615);
+    text(plusOne,410,615);
+    text(plusOne,550,615);
     text("Milk",270,490);
     text("Espresso",395,490);
     text("Syrup",545,490);
-    text("Milk:? | Espresso: 3 | Syrup: 1", 50,65);
+    text("? Parts Milk | 3 Parts Espresso | 1 Part Syrup", 50,60);
+    text("8 Total Parts", 50,85);
 
     textSize(30);
     text("8",170,490);
     text(timer,565,85);
     textSize(12);
+    text("Total Parts", 150,505);
 
     if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
       timer --;
     }
     if (timer == 0) {
-      text("TOO SLOW, NEXT ORDER!",50,125);
+      text("TOO SLOW, NEXT ORDER!",50,137);
     }
 
     if(mouseX > 220 && mouseX < 350 && mouseY > 500 && mouseY < 610){
@@ -169,26 +180,32 @@ if(mouseX > 470 && mouseX < 630 && mouseY > 500 && mouseY < 610){
 }
 }else if(currentImage == 1){
     image(cafeScene,0,0);
-    image(lattZero,80,500);
+    image(latteStages[lattClick],80,500);
     image(milk,230,500);
     image(espresso,370,500);
     image(milkFoamer,505,500);
+    text(plusOne,270,615);
+    text(plusOne,410,615);
+    text(plusOne,550,615);
     text("Latte",115,615);
     text("Milk",270,490);
     text("Espresso",395,490);
     text("Milk Foam",525,490);
-    text("Milk:1 | Espresso: ? | Milk Foam: 4", 50,65);
+    text("1 Part Milk | ? Parts Espresso", 50,55);
+    text("4 Parts Milk Foam",50,75);
+    text("7 Total Parts", 50,95);
 
     textSize(30);
     text("7",170,490);
     text(timerTwo,560,85);
     textSize(12);
+    text("Total Parts", 150,505);
 
     if (frameCount % 60 == 0 && timerTwo > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
       timerTwo --;
     }
     if (timerTwo == 0) {
-      text("TOO SLOW, NEXT ORDER!",50,125);
+      text("TOO SLOW, NEXT ORDER!",50,137);
     }
 
     if(mouseX > 220 && mouseX < 350 && mouseY > 500 && mouseY < 610){
@@ -203,29 +220,37 @@ if(mouseX > 470 && mouseX < 630 && mouseY > 500 && mouseY < 610){
     // text("MILK FOAM WORKING",100,100);
 }
 
+
+
 }else if(currentImage == 2){
   image(cafeScene,0,0);
-  image(glacZero,80,500);
+  image(glacStages[glacClick],80,500);
   image(espresso,230,500);
   image(iceCream,370,500);
   image(whippedCream,505,500);
+  text(plusOne,270,615);
+  text(plusOne,410,615);
+  text(plusOne,550,615);
   text("Cafe Glace",100,615);
   text("Espresso",255,490);
   text("Ice Cream",390,490);
   text("Whipped Cream",515,490);
-  text("Espresso:3 | Ice Cream: 4", 50,55);
-  text("Whipped Cream: ?",50,75);
+  text("3 Parts Espresso | 4 Parts Ice Cream", 50,55);
+  text("? Parts Whipped Cream",50,75);
+  text("9 Total Parts", 50,95);
+
 
   textSize(30);
   text("9",170,490);
   text(timerThree,560,85);
   textSize(12);
+  text("Total Parts", 150,510);
 
   if (frameCount % 60 == 0 && timerThree > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
     timerThree --;
   }
   if (timerThree == 0) {
-    text("TOO SLOW, NEXT ORDER!",50,125);
+    text("TOO SLOW, NEXT ORDER!",50,137);
   }
 
   if(mouseX > 220 && mouseX < 350 && mouseY > 500 && mouseY < 610){
@@ -243,11 +268,32 @@ if(mouseX > 470 && mouseX < 630 && mouseY > 500 && mouseY < 610){
 }else if(currentImage == 3){
   image(instructions,0,0);
 
+
 }
 
-//barista art
-// fill(baristaColor);
-// ellipse(200,50,50,50);
+
+
+
+////////change
+
+
+if(answer == "Correct"){
+  text(answer,50,137);
+
+    if(millis() - prevTime >= interval){
+      answer = "";
+      prevTime = millis();
+    }
+}
+
+if(answer == "Try Again"){
+  text(answer, 50,137);
+
+    if(millis() - prevTime >= interval){
+      answer = "";
+      prevTime = millis();
+    }
+}
 
 }
 
@@ -261,12 +307,10 @@ if(currentImage != -1){
   // text("MILK WORKING",100,100);
   milkCounter++;
   machClick++;
-  if (machClick > 1 && machClick % 2 == 0){
-    image(machStages[1],80,500);
-    console.log(machClick);
-    console.log("machClick");
+
+  if(machClick >= machStages.length){
+    machClick = machStages.length -1;
   }
-  //coffee++
   console.log(milkCounter);
 
 }
@@ -274,11 +318,24 @@ if(currentImage != -1){
   if(mouseX > 350 && mouseX < 470 && mouseY > 500 && mouseY < 610){
   // text("ESPRESSO WORKING",100,100);
   espressoCounter++;
+  lattClick++;
+
+  if(lattClick >= latteStages.length){
+    lattClick = latteStages.length -1;
+  }
+  console.log(espressoCounter);
 }
 
 if(mouseX > 470 && mouseX < 630 && mouseY > 500 && mouseY < 610){
 // text("SYRUP WORKING",100,100);
 syrupCounter++;
+glacClick++;
+
+if(glacClick >= glacStages.length){
+  glacClick = glacStages.length -1;
+}
+console.log(syrupCounter);
+
 }
 
   if(coffeeType[currentImage][0] == milkCounter && coffeeType[currentImage][1] == espressoCounter && coffeeType[currentImage][2] == syrupCounter){
@@ -290,6 +347,9 @@ syrupCounter++;
 
     //go to next stage //
     currentImage = currentImage + 1;
+    ////////change
+
+      prevTime = millis();
 
 
   }else{
@@ -303,6 +363,8 @@ syrupCounter++;
         syrupCounter = 0;
 
         startTimer = true;
+        ////////change
+
         prevTime = millis();
     }
   }
